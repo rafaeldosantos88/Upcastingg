60712582642686edf94e694045e8e02bdf4e7f4b
Rafael dos Santos <raphael_979@hotmail.com>
2019-10-09 16:09:25 -0300

    Foi criado subclasse BussinessAccount(Conta empresa)

---
+++ Entidades/BussinessAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Upcasting.Entidades
+{
+    class BusinessAccount : Account                   //Conta Empresa.Foi colocado ":" Account para poder fazer herança
+    {
+        public double LoanLimit { get; set; }       //Limite para emprestimo(LoanLimit)
+
+        public BusinessAccount()
+        {
+
+        }
+        public BusinessAccount(int number, string holder, double balance, double loanLimit)
+            : base(number, holder, balance)         //Aqui estou aproveitando o construtor da classe Account
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount) //Operação emprestimo
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+
+    }
+}
+
