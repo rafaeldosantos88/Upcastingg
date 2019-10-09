8164209c6d61c27dbd472002e81316b2fb299478
Rafael dos Santos <raphael_979@hotmail.com>
2019-10-09 16:01:36 -0300

    Foi criado superclasse Account(Conta)

---
+++ Entidades/Account.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Upcasting.Entidades
+{
+    class Account    //Classe conta
+    {
+        public int Number { get; set; }        // Numero Conta
+        public string Holder { get; set; }    //Titular conta
+        public double Balance { get; protected set; }  //Saldo da conta
+
+        public Account()                    //Construtor padrão
+        {
+
+        }
+
+        public Account(int number, string holder, double balance) //Construtor com argumentos
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+        public void withdraw(double amount) //Operação Saque(WIthdraw) recebendo uma quantia(amount)
+        {
+            Balance -= amount; //Para fazer o saque fazemos o Calculo Balance(saldo)- amount(quantia)
+
+        }
+        public void Deposit(double amount) // Operação deposito
+        {
+            Balance += amount;
+
+        }
+
+    }
+}
