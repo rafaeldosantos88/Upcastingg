84cc53249d728aaff3d09cdaa31fa4f5b2e034cc
Rafael dos Santos <raphael_979@hotmail.com>
2019-10-09 16:17:25 -0300

    FOI CRIADO A SUBCLASSE SavingsAccount

---
+++ Entidades/SavingsAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Upcasting.Entidades
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; } //TAXA DE JUROS
+
+        public SavingsAccount()
+        {
+
+        }
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = InterestRate;
+        }
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate; // Vai atualizar o saldo da conta baseado na taxa de juros InteresRate
+        }
+    }
+}
